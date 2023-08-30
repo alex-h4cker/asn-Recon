6ce1d09c421a694f472f3759727eafff6a3baa11
Alex <130258074+alex-h4cker@users.noreply.github.com>
2023-08-30 10:38:53 -0400

    Update asnrecon.sh

--- asnrecon.sh
+++ asnrecon.sh
@@ -1,17 +1,12 @@
-!/bin/bash
-
+#!/bin/bash
 
 read -p "Enter search term: " searchTerm
 
+encodedSearchTerm=$(echo -n "$searchTerm" | xxd -plain | tr -d '\n')
 
-encodedSearchTerm=$(echo -n "$searchTerm" | xxd -plain | tr -d '\n' | sed 's/\(>
-
-
-curl "https://bgp.he.net/search?search%5Bsearch%5D=$encodedSearchTerm -o asn.txt
-
+curl "https://bgp.he.net/search?search%5Bsearch%5D=$encodedSearchTerm" -o as.txt
 
 grep -oE 'AS[0-9]+' as.txt
-grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+' asa.txt
-
+grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+' as.txt
 
 rm as.txt
